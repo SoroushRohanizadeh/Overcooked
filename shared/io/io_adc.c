4e2ab76a175dd5e0ea6c1f27c6bad54d1f0392f5
sRohani <rohanisoroush@gmail.com>
2024-07-22 13:17:16 -0700

    saving adc code cuz there's include errors

--- shared/io/io_adc.c
+++ shared/io/io_adc.c
@@ -3,3 +3,22 @@
 //
 
 #include "io_adc.h"
+ADC_HandleTypeDef hadc1;
+ADC_HandleTypeDef hadc2;
+
+enum ADC_Pins* pins;
+uint8_t pinCount;
+
+
+int io_adc_init(ADC_HandleTypeDef* hadcs, bool twoHandles, enum ADC_Pins *adcPins, uint8_t numPins) {
+    hadc1 = hadcs[0];
+    if (twoHandles) {
+        hadc2 = hadcs[1];
+    }
+
+    pinCount = numPins;
+    pins = adcPins;
+}
+
+int io_adc_read(enum ADC_Pins pin) {
+}
--- shared/io/io_adc.h
+++ shared/io/io_adc.h
@@ -1,5 +1,22 @@
 #ifndef IO_ADC_H
 #define IO_ADC_H
+#include <stdbool.h>
+#include <stm32f1xx_hal_adc.h>
+
+enum ADC_Pins {
+    PA0,
+    PA1,
+    PA2,
+    PA3,
+    PA4,
+    PA5,
+    PA6,
+    PA7,
+    PB0,
+    PB1
+};
 
 #endif //IO_ADC_H
-io_adc_read();
+io_adc_init(ADC_HandleTypeDef* hadcs, bool numhadc, enum ADC_Pins* adcPins, uint8_t numPins);
+
+io_adc_read(enum ADC_Pins pin);
